bd9522a4a5daa9285b6d782323cb56fe292c4c99
Matheus Gomes <>
2021-04-07 22:51:17 +0000

    bitsc

--- datalab-handout/bits.c
+++ datalab-handout/bits.c
@@ -1,7 +1,7 @@
 /*
  * CS:APP Data Lab
  *
- * <Please put your name and userid here>
+ * <Matheus Gomes e Silva>
  *
  * bits.c - Source file with your solutions to the Lab.
  *          This is the file you will hand in to your instructor.
@@ -17,113 +17,6 @@
 #include "common.h"
 #endif
 
-#if 0
-/*
- * Instructions to Students:
- *
- * STEP 1: Read the following instructions carefully.
- */
-
-You will provide your solution to the Data Lab by
-editing the collection of functions in this source file.
-
-INTEGER CODING RULES:
-
-  Replace the "return" statement in each function with one
-  or more lines of C code that implements the function. Your code
-  must conform to the following style:
-
-  int Funct(arg1, arg2, ...) {
-      /* brief description of how your implementation works */
-      int var1 = Expr1;
-      ...
-      int varM = ExprM;
-
-      varJ = ExprJ;
-      ...
-      varN = ExprN;
-      return ExprR;
-  }
-
-  Each "Expr" is an expression using ONLY the following:
-  1. Integer constants 0 through 255 (0xFF), inclusive. You are
-     not allowed to use big constants such as 0xffffffff.
-     However, you are allowed to combine constants to values greater
-     than 255 or less than 0. e.g. 250 + 250 = 500, so long as
-     the operator you are using to combine the constants is listed as
-     "legal" at the top of the method you are writing.
-  2. Function arguments and local variables (no global variables).
-  3. Any operators listed as "legal" at the top of the method you are writing.
-  4. Shorthand versions of "legal" operators (ex. you can use ++ and += if + is legal)
-
-  Some of the problems restrict the set of allowed operators even further.
-  Each "Expr" may consist of multiple operators. You are not restricted to
-  one operator per line.
-
-  You are expressly forbidden to:
-  1. Use any control constructs such as if, do, while, for, switch, etc.
-  2. Define or use any macros.
-  3. Define any additional functions in this file.
-  4. Call any functions.
-  5. Use any other operations, such as &&, ||, -, or ?:
-  6. Use any form of casting.
-  7. Use any data type other than int.  This implies that you
-     cannot use arrays, structs, or unions.
-
-
-  You may assume that your machine:
-  1. Uses 2s complement, 32-bit representations of integers.
-  2. Performs right shifts arithmetically.
-  3. Has unpredictable behavior when shifting if the shift amount
-     is less than 0 or greater than 31.
-
-
-EXAMPLES OF ACCEPTABLE CODING STYLE:
-  /*
-   * pow2plus1 - returns 2^x + 1, where 0 <= x <= 31
-   */
-  int pow2plus1(int x) {
-     /* exploit ability of shifts to compute powers of 2 */
-     return (1 << x) + 1;
-  }
-
-  /*
-   * pow2plus4 - returns 2^x + 4, where 0 <= x <= 31
-   */
-  int pow2plus4(int x) {
-     /* exploit ability of shifts to compute powers of 2 */
-     int result = (1 << x);
-     result += 4;
-     return result;
-  }
-
-
-NOTES:
-  1. Use the dlc (data lab checker) compiler (described in the handout) to
-     check the legality of your solutions.
-  2. Each function has a maximum number of operations (integer, logical,
-     or comparison) that you are allowed to use for your implementation
-     of the function.  The max operator count is checked by dlc.
-     Note that assignment ('=') is not counted; you may use as many of
-     these as you want without penalty.
-  3. Use the btest test harness to check your functions for correctness.
-  4. The maximum number of ops for each function is given in the
-     header comment for each function. If there are any inconsistencies
-     between the maximum ops in the writeup and in this file, consider
-     this file the authoritative source.
-  5. Write comments for your functions! These won't be graded, but it's good
-     practice to write comments and may help you debug your code. See the
-     above example to see what the comments should look like.
-
-/*
- * STEP 2: Modify the following functions according the coding rules.
- *
- *   IMPORTANT. TO AVOID GRADING SURPRISES:
- *   1. Use the dlc compiler to check that your solutions conform
- *      to the coding rules.
- */
-#endif
-
 /* 
  * bitXor - x^y using only ~ and & 
  *   Example: bitXor(4, 5) = 1
@@ -132,7 +25,11 @@ NOTES:
  *   Rating: 1
  */
 int bitXor(int x, int y) {
-  return 2;
+  int z;
+  z = x & y;
+
+
+  return ~(~x & ~y) & ~z;
 }
 
 /* 
@@ -144,7 +41,12 @@ int bitXor(int x, int y) {
  *   Rating: 2
  */
 int allEvenBits(int x) {
-  return 2;
+  int byteref;
+  //isolo a metade do MSB para comparação
+  byteref = 0xff;
+  
+  /*por fim fazer uma comparação(xor) do numero resultante com a negação de x>>28, por fim negando toda a operação */
+  return (~x ^ byteref);
 }
 
 /* 
@@ -156,7 +58,10 @@ int allEvenBits(int x) {
  *   Rating: 3 
  */
 int logicalShift(int x, int n) {
-    return 2;
+
+    x = (x + x)>>4 ^ x;
+  
+    return x;
 }
 
 /* 
